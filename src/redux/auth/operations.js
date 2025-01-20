1cde8aa2f841e8770232379eae973fd78fcd09d4
Pavlo-Chernicenko <pwned7067@gmail.com>
2025-01-20 22:51:54 +0200

    Added a constant for paths

--- src/redux/auth/operations.js
+++ src/redux/auth/operations.js
@@ -3,6 +3,13 @@ import axios from 'axios';
 
 axios.defaults.baseURL = import.meta.env.VITE_BASE_URL;
 
+const USER_API_PATHS = {
+  register: '/users/signup',
+  login: '/users/login',
+  logout: '/users/logout',
+  currentUser: '/users/current',
+};
+
 const setAuthHeader = token => {
   axios.defaults.headers.common.Authorization = `Bearer ${token}`;
 };
@@ -15,7 +22,7 @@ export const register = createAsyncThunk(
   'auth/register',
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post('/users/signup', credentials);
+      const res = await axios.post(USER_API_PATHS.register, credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -28,7 +35,7 @@ export const logIn = createAsyncThunk(
   'auth/login',
   async (credentials, thunkAPI) => {
     try {
-      const res = await axios.post('/users/login', credentials);
+      const res = await axios.post(USER_API_PATHS.login, credentials);
       setAuthHeader(res.data.token);
       return res.data;
     } catch (error) {
@@ -39,7 +46,7 @@ export const logIn = createAsyncThunk(
 
 export const logOut = createAsyncThunk('auth/logout', async (_, thunkAPI) => {
   try {
-    await axios.post('/users/logout');
+    await axios.post(USER_API_PATHS.logout);
     clearAuthHeader();
   } catch (error) {
     return thunkAPI.rejectWithValue(error.message);
@@ -58,7 +65,7 @@ export const refreshUser = createAsyncThunk(
 
     try {
       setAuthHeader(persistedToken);
-      const res = await axios.get('/users/current');
+      const res = await axios.get(USER_API_PATHS.currentUser);
       return res.data;
     } catch (error) {
       return thunkAPI.rejectWithValue(error.message);
